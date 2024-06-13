fdb965ca9f94f47bbaac0e02db2dfa15559ccbc1
elkcert <80402248+elkcert@users.noreply.github.com>
2024-06-13 05:36:21 +0200

    chore: Configure shadcn/ui

--- project/lesotho/frontend/vite.config.ts
+++ project/lesotho/frontend/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  /* For shadcn/ui https://ui.shadcn.com/docs/installation/vite#update-viteconfigts */
+  resolve: {
+    alias: {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
