4fc1dda037dcd7c492ae36b5761a00b4bd874346
elkcert <80402248+elkcert@users.noreply.github.com>
2024-06-15 09:54:43 +0200

    feat: Disable UI while fetching response

--- project/lesotho/frontend/src/App.tsx
+++ project/lesotho/frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 import { ModeToggle } from "@/components/mode-toggle"
 import { Input } from "./components/ui/input"
 import { X } from "lucide-react"
-import { useState } from "react"
+import { useRef, useState } from "react"
 import { aclCheck, aclUpdate, namespaceUpdate } from "./http/endpoints"
 import { useToast } from "./components/ui/use-toast"
 import axios, { AxiosError } from "axios"
@@ -13,8 +13,11 @@ function App() {
   const [object, setObject] = useState("")
   const [relation, setRelation] = useState("")
   const [user, setUser] = useState("")
+  const [aclLocked, setAclLocked] = useState(false);
 
+  const inputFile = useRef<HTMLInputElement | null>(null)
   const [namespaceFile, setNamespaceFile] = useState<File | null>(null)
+  const [namespaceLocked, setNamespaceLocked] = useState(false)
 
   const { toast } = useToast()
 
@@ -26,6 +29,7 @@ function App() {
   }
 
   const checkACL = () => {
+    setAclLocked(true)
     const namespaceObject = `${namespace}:${object}`
     aclCheck({object: namespaceObject, relation, user})
       .then(res => toast({
@@ -49,9 +53,11 @@ function App() {
           console.log(err.message)
         }
       })
+      .finally(() => setAclLocked(false))
   }
 
   const updateACL = () => {
+    setAclLocked(true)
     const namespaceObject = `${namespace}:${object}`
     aclUpdate({object: namespaceObject, relation, user})
       .then(() => toast({
@@ -74,6 +80,7 @@ function App() {
           console.log(err.message)
         }
       })
+      .finally(() => setAclLocked(false))
   }
 
   const attemptStringtoJSON = (val: string): unknown => {
@@ -88,6 +95,7 @@ function App() {
     if (namespaceFile === null) {
       return
     }
+    setNamespaceLocked(true)
     const reader = new FileReader()
     reader.onload = () => {
       const namespace = reader.result as string
@@ -98,13 +106,20 @@ function App() {
           variant: "destructive",
           description: `${namespaceFile.name} is not a valid JSON file`,
         })
+        setNamespaceLocked(false)
         return
       }
       namespaceUpdate(json)
-        .then(() => toast({
-          title: "Namespace Update result",
-          description: "Successfully updated",
-        }))
+        .then(() => {
+          toast({
+            title: "Namespace Update result",
+            description: "Successfully updated",
+          })
+          setNamespaceFile(null)
+          if (inputFile.current) {
+            inputFile.current.value = ""
+          }
+        })
         .catch((err: Error | AxiosError) => {
           if (axios.isAxiosError(err) && err.response?.status === 400) {
             toast({
@@ -121,6 +136,9 @@ function App() {
             console.log(err.message)
           }
         })
+        .finally(() => {
+          setNamespaceLocked(false)
+        })
     }
     reader.readAsText(namespaceFile)
   }
@@ -135,24 +153,24 @@ function App() {
           <h2>ACL</h2>
           <div className="flex gap-2">
             <div className="flex">
-              <Input value={namespace} onChange={e => setNamespace(e.target.value)} placeholder="namespace" className="w-[100px] rounded-r-none focus:z-10 focus:rounded-md"></Input>
-              <Input value={object} onChange={e => setObject(e.target.value)} placeholder="object" className="w-[150px] rounded-none focus:z-10 focus:rounded-md"></Input>
-              <Input value={relation} onChange={e => setRelation(e.target.value)} placeholder="relation" className="w-[150px] rounded-none focus:z-10 focus:rounded-md"></Input>
-              <Input value={user} onChange={e => setUser(e.target.value)} placeholder="user" className="w-[150px] rounded-l-none focus:z-10 focus:rounded-md"></Input>
+              <Input disabled={aclLocked} value={namespace} onChange={e => setNamespace(e.target.value)} placeholder="namespace" className="w-[100px] rounded-r-none focus:z-10 focus:rounded-md"></Input>
+              <Input disabled={aclLocked} value={object} onChange={e => setObject(e.target.value)} placeholder="object" className="w-[150px] rounded-none focus:z-10 focus:rounded-md"></Input>
+              <Input disabled={aclLocked} value={relation} onChange={e => setRelation(e.target.value)} placeholder="relation" className="w-[150px] rounded-none focus:z-10 focus:rounded-md"></Input>
+              <Input disabled={aclLocked} value={user} onChange={e => setUser(e.target.value)} placeholder="user" className="w-[150px] rounded-l-none focus:z-10 focus:rounded-md"></Input>
             </div>
-            <Button variant="outline" size="icon" onClick={emptyInputs}><X/></Button>
+            <Button disabled={aclLocked} variant="outline" size="icon" onClick={emptyInputs}><X/></Button>
           </div>
           <div className="flex gap-1">
-            <Button onClick={() => checkACL()}>Check</Button>
-            <Button onClick={() => updateACL()}>Update</Button>
+            <Button disabled={aclLocked} onClick={() => checkACL()}>Check</Button>
+            <Button disabled={aclLocked} onClick={() => updateACL()}>Update</Button>
           </div>
         </div>
         <Separator/>
         <div className="p-4 flex flex-col gap-2">
           <h2>Namespace</h2>
           <div className="flex gap-2">
-            <Input type="file" className="w-[300px]" onChange={e => setNamespaceFile(e.target.files?.item(0) ?? null)}/>
-            <Button onClick={() => updateNamespace()}>Update</Button>
+            <Input ref={inputFile} disabled={namespaceLocked} type="file" className="w-[300px]" onChange={e => setNamespaceFile(e.target.files?.item(0) ?? null)}/>
+            <Button disabled={namespaceLocked} onClick={() => updateNamespace()}>Update</Button>
           </div>
         </div>
       </div>
