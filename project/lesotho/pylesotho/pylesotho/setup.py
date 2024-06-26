7f02abc96fbf80d4cb9bd1e083ec50e95638006d
magley <60552482+magley@users.noreply.github.com>
2024-06-26 11:07:15 +0200

    feat: Add HTTPs support for `pylesotho`

--- project/lesotho/pylesotho/pylesotho/setup.py
+++ project/lesotho/pylesotho/pylesotho/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pylesotho',
-    version='0.1',
+    version='0.2',
     description='A Python client for Lesotho authorization server',
     author='magley, elkcert',
     packages=find_packages(),
